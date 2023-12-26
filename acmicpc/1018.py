cc039c8e3f6cd9bf8bcbeba29c29e8c041875ef8
상원 <Admin@sang-won-ui-MacBookAir.local>
2023-12-26 09:52:27 +0900

    missed name change

--- acmicpc/1018.py
+++ acmicpc/1018.py
@@ -1,28 +1,25 @@
-n, m = map(int, input().split())
-board = []
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+board = [sys.stdin.readline().strip() for _ in range(n)]
 result = []
 
-for i in range(n):
-    board.append(input())
-
 for a in range(n-7):
-    for b in range(n-7):
+    for b in range(m-7):
         whiteindex = 0
         blackindex = 0
         for i in range(a,a+8):
             for j in range(b,b+8):
                 if (i+j)%2 == 0:
-                    if board[i][j]!='W':
+                    if board[i][j] != 'W':
                         whiteindex += 1
-                    else:
+                    if board[i][j] != 'B':
                         blackindex += 1
                 else:
-                    if board[i][j]!='W':
-                        blackindex += 1
-                    else:
+                    if board[i][j] != 'B':
                         whiteindex += 1
-        
-        result.append(whiteindex)
-        result.append(blackindex)
-        
+                    if board[i][j] != 'W':
+                        blackindex += 1
+        result.append(min(whiteindex, blackindex))
+
 print(min(result))
